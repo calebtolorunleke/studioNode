076a70429311a51b0045ffc7b2d41b2eebd45bd0
Caleb Adebayo <calebtolorunleke@yahoo.com>
2025-05-28 23:53:21 +0100

    node for tech studio

---
+++ utility/dev.js
@@ -0,0 +1,6 @@
+// require(path) 
+const { val, val2 } = require('./developer')
+const { name } = require('./developer')
+console.log(val, val2)
+
+console.log(name)
---
+++ utility/developer.js
@@ -0,0 +1,8 @@
+const val = 67
+const val2 = 57
+
+// module.exports = { val, val2 }
+
+const name = 'john'
+
+module.exports = { val, val2, name }
